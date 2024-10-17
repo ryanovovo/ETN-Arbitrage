ed30fa269026bb9baea157ad5dcd3c668778a246
ryanovovo <ryanovovo@gmail.com>
2024-10-18 03:43:11 +0800

    Refactor State class to update frame data and calculate arbitrage opportunities

--- backend/state.py
+++ backend/state.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from backend.frame import Frame
 from backend.utils import get_data_type
+from backend.arbitrage import Arbitrage
 
 
 class State:
@@ -26,4 +27,12 @@ class State:
     def update_frame(self, data):
         _, category = get_data_type(data)
         code = data.code
-        self.frames[code][category].data_to_frame(data)
+        self.frames[code][category].update_frame(data)
+    
+    def get_arbitrage(self, stock_code, future_code):
+        stock_frame = self.get_frame(stock_code, 'stk')
+        future_frame = self.get_frame(future_code, 'fop')
+        arbitrage = Arbitrage(stock_frame=stock_frame, future_frame=future_frame)
+        return arbitrage
+
+        
