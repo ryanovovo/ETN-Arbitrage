5d29428f3e8c20e0677f471406e977cfd87eabd8
ryanovovo <ryanovovo@gmail.com>
2024-10-18 03:42:53 +0800

    Refactor callback functions to update frames and calculate arbitrage opportunities

--- backend/callback_functions.py
+++ backend/callback_functions.py
@@ -1,26 +1,65 @@
 from pprint import pprint
 from backend.utils import get_nearmonth_future_code
+from frontend.message import arbitrage_to_embed
 
 
 def callback_etn(api, data_manager, args, kwargs):
-    data = data_manager.get_latest_data('020039', 'stk', 'quote')
-    data = dict(data)
-    pprint(data, sort_dicts=False)
+    nearmonth_future_code = get_nearmonth_future_code(api, 'TXFR1')
+    stock_data = data_manager.get_latest_data('020039', 'stk', 'quote')
+    future_data = data_manager.get_latest_data(nearmonth_future_code, 'fop', 'tick')
+
+    state = kwargs['state']
+    state.update_frame(stock_data)
+    state.update_frame(future_data)
+    stock_frame = state.get_frame('020039', 'stk')
+    future_frame = state.get_frame(nearmonth_future_code, 'fop')
+
+    webhook_manager = kwargs['webhook_manager']
+    arbitrage = kwargs['arbitrage']
+    arbitrage.set_stock_frame(stock_frame)
+    arbitrage.set_future_frame(future_frame)
+    arbitrage.calculate_arbitrage()
+    webhook_manager.send_embed_message(arbitrage)
+    # pprint(dict(stock_frame), sort_dicts=False)
 
 
 def callback_tx_tick(api, data_manager, args, kwargs):
     nearmonth_future_code = get_nearmonth_future_code(api, 'TXFR1')
-    data = data_manager.get_latest_data(nearmonth_future_code, 'fop', 'tick')
-    state = kwargs['state']
-    state.update_frame(data)
-    frame = state.get_frame(nearmonth_future_code, 'fop')
-    pprint(dict(frame), sort_dicts=False)
+    stock_data = data_manager.get_latest_data('020039', 'stk', 'quote')
+    future_data = data_manager.get_latest_data(nearmonth_future_code, 'fop', 'tick')
 
+    state = kwargs['state']
+    stock_frame = state.get_frame('020039', 'stk')
+    future_frame = state.get_frame(nearmonth_future_code, 'fop')
+    state.update_frame(stock_data)
+    state.update_frame(future_data)
+    stock_frame = state.get_frame('020039', 'stk')
+    future_frame = state.get_frame(nearmonth_future_code, 'fop')
+    
+    webhook_manager = kwargs['webhook_manager']
+    arbitrage = kwargs['arbitrage']
+    arbitrage.set_stock_frame(stock_frame)
+    arbitrage.set_future_frame(future_frame)
+    arbitrage.calculate_arbitrage()
+    webhook_manager.send_embed_message(arbitrage)
+    # pprint(dict(future_frame), sort_dicts=False)
 
 def callback_tx_bidask(api, data_manager, args, kwargs):
     nearmonth_future_code = get_nearmonth_future_code(api, 'TXFR1')
-    data = data_manager.get_latest_data(nearmonth_future_code, 'fop', 'bidask')
+    stock_data = data_manager.get_latest_data('020039', 'stk', 'quote')
+    future_data = data_manager.get_latest_data(nearmonth_future_code, 'fop', 'bidask')
+
     state = kwargs['state']
-    state.update_frame(data)
-    frame = state.get_frame(nearmonth_future_code, 'fop')
-    pprint(dict(frame), sort_dicts=False)
+    state.update_frame(stock_data)
+    state.update_frame(future_data)
+    stock_frame = state.get_frame('020039', 'stk')
+    future_frame = state.get_frame(nearmonth_future_code, 'fop')
+    
+    webhook_manager = kwargs['webhook_manager']
+    arbitrage = kwargs['arbitrage']
+    arbitrage.set_stock_frame(stock_frame)
+    arbitrage.set_future_frame(future_frame)
+    arbitrage.calculate_arbitrage()
+    webhook_manager.send_embed_message(arbitrage)
+    # print(future_data)
+    # pprint(dict(future_frame), sort_dicts=False)
