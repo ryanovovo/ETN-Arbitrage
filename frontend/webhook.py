477f3aa2b704e8c325459f3df18012a11ee5ec8d
ryanovovo <ryanovovo@gmail.com>
2024-10-18 11:03:11 +0800

    Refactor webhook.py to update send_embed_message function and improve code readability

--- frontend/webhook.py
+++ frontend/webhook.py
@@ -3,18 +3,18 @@ import os
 import dotenv
 import logging
 from threading import RLock
-from frontend.message import arbitrage_to_embed
+from frontend.message import state_to_embed
 class WebhookManager:
     def __init__(self):
         dotenv.load_dotenv()
         self.webhook_url = os.getenv('WEBHOOK_URL')
-        self.last_sent_message = None
+        self.last_sent_state = None
         self.lock = RLock()
         if self.webhook_url is None:
             raise ValueError("WEBHOOK_URL is required")
 
-    def send_embed_message(self, arbitrage):
-        embed = arbitrage_to_embed(arbitrage)
+    def send_embed_message(self, state):
+        embed = state_to_embed(state)
         embed_dict = embed.to_dict()
         data_message = {
             'embeds': [embed_dict]
@@ -23,9 +23,9 @@ class WebhookManager:
             "Content-Type": "application/json"
         }
         with self.lock:
-            if not self.need_send(arbitrage):
+            if not self.need_send(state):
                 return True, None
-            self.last_sent_message = arbitrage
+            self.last_sent_state = state
         response = requests.post(self.webhook_url, json=data_message, headers=headers)
         if response.status_code != 204:
             logging.error(f"Failed to send message: {response.text}")
@@ -33,11 +33,14 @@ class WebhookManager:
 
         return True, None
     
-    def need_send(self, arbitrage):
-        if self.last_sent_message is None:
+    def need_send(self, state):
+        if self.last_sent_state is None:
             return True
-        if arbitrage.action != self.last_sent_message.action and arbitrage.action != None:
+        if state.action != self.last_sent_state.action and state.action != None:
             return True
-        if arbitrage.action_price != self.last_sent_message.action_price and arbitrage.action_price != None:
+        if state.action_price != self.last_sent_state.action_price and state.action_price != None:
             return True
+        if state.expected_price != self.last_sent_state.expected_price and state.expected_price != None:
+            return True
+        # For debugging
         return False
