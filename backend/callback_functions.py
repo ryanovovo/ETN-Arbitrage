fc9f56ffcd821e913a0a41f274bc926f4664c91f
ryanovovo <ryanovovo@gmail.com>
2024-10-19 02:19:12 +0800

    Refactor callback_functions.py to update callback_update logic

--- backend/callback_functions.py
+++ backend/callback_functions.py
@@ -1,37 +1,13 @@
-from pprint import pprint
 from backend.utils import get_nearmonth_future_code
-from frontend.message import state_to_embed
-import json
-
-def update(kwargs, stock_data, future_data):
-    state = kwargs['state']
-    state.update_frame(stock_data)
-    state.update_frame(future_data)
-
-    webhook_manager = kwargs['webhook_manager']
-    webhook_manager.send_embed_message(state)
-    # pprint(dict(future_frame), sort_dicts=False)
 
 
-def callback_etn(api, data_manager, args, kwargs):
-    nearmonth_future_code = get_nearmonth_future_code(api, 'TXFR1')
-    stock_data = data_manager.get_latest_data('020039', 'stk', 'quote')
-    future_data = data_manager.get_latest_data(nearmonth_future_code, 'fop', 'tick')
-    update(kwargs, stock_data, future_data)
-    # pprint(dict(stock_frame), sort_dicts=False)
-
-
-def callback_tx_tick(api, data_manager, args, kwargs):
-    nearmonth_future_code = get_nearmonth_future_code(api, 'TXFR1')
-    stock_data = data_manager.get_latest_data('020039', 'stk', 'quote')
-    future_data = data_manager.get_latest_data(nearmonth_future_code, 'fop', 'tick')
-    update(kwargs, stock_data, future_data)
-
-
-def callback_tx_bidask(api, data_manager, args, kwargs):
-    nearmonth_future_code = get_nearmonth_future_code(api, 'TXFR1')
-    stock_data = data_manager.get_latest_data('020039', 'stk', 'quote')
-    future_data = data_manager.get_latest_data(nearmonth_future_code, 'fop', 'bidask')
-    update(kwargs, stock_data, future_data)
-    future_frame = kwargs['state'].future_frame
+def callback_update(data, args, kwargs):
+    try:
+        state = kwargs['state']
+    except KeyError:
+        raise ValueError("State is required")
+    state.update_frame(data)
+    webhook_manager = kwargs.get('webhook_manager')
+    if webhook_manager is not None:
+        webhook_manager.send_embed_message(state)
     # pprint(dict(future_frame), sort_dicts=False)
