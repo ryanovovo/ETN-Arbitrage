c86c990b64dd85c646edb650fb72f35fe47f2d96
ryanovovo <ryanovovo@gmail.com>
2024-10-19 03:09:38 +0800

    Refactor QuoteManager to handle SIGINT signal and gracefully shut down Redis server

--- backend/quote.py
+++ backend/quote.py
@@ -2,6 +2,7 @@ import asyncio
 from backend.callback import CallbackManager
 from backend.data_redis import DataManager
 import logging
+import signal
 
 
 class QuoteManager:
@@ -29,9 +30,10 @@ class QuoteManager:
         self.api.quote.set_on_quote_stk_v1_callback(
             self.on_data_received('stk', 'quote')
         )
+        self.loop.add_signal_handler(signal.SIGINT, self.__del__)
 
     def __del__(self):
-        self.clear_all_callbacks()
+        self.data_manager.shutdown_redis()
 
     def on_data_received(self, category: str, data_type: str):
         def handler(_exchange, data):
