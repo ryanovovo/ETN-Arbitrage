61bd70bca103a9810a1ad1183ea6e887c030c9c2
ryanovovo <ryanovovo@gmail.com>
2024-10-18 11:01:40 +0800

    Refactor main.py to simplify code structure and remove unused imports and variables

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from backend.state import State
 from backend.callback_functions import callback_tx_tick, callback_tx_bidask, callback_etn
 from frontend.webhook import WebhookManager
 from backend.arbitrage import Arbitrage
-from frontend.message import arbitrage_to_embed
 
 logging.basicConfig(filename='./logs/shioaji.log', level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s')
@@ -19,21 +18,16 @@ quote_manager = QuoteManager(api, loop)
 nearmonth_future_code = get_nearmonth_future_code(api, 'TXFR1')
 
 etn_frame = Frame(api, snapshot_init=True, code='020039', category='stk')
-etn_frame.update_close()
 future_frame = Frame(api, snapshot_init=True, code=nearmonth_future_code, category='fop')
-future_frame.update_close()
-state = State()
-state.add_frame(etn_frame)
-state.add_frame(future_frame)
+state = State(stock_frame=etn_frame, future_frame=future_frame)
 webhook_manager = WebhookManager()
-arbitrage = Arbitrage(stock_frame=etn_frame, future_frame=future_frame)
 
 quote_manager.subscribe(nearmonth_future_code, 'fop', 'tick')
 quote_manager.subscribe(nearmonth_future_code, 'fop', 'bidask')
-quote_manager.add_callback(nearmonth_future_code, 'fop', 'tick', callback_tx_tick, state=state, webhook_manager=webhook_manager, arbitrage=arbitrage)
-quote_manager.add_callback(nearmonth_future_code, 'fop', 'bidask', callback_tx_bidask, state=state, webhook_manager=webhook_manager, arbitrage=arbitrage)
+quote_manager.add_callback(nearmonth_future_code, 'fop', 'tick', callback_tx_tick, state=state, webhook_manager=webhook_manager)
+quote_manager.add_callback(nearmonth_future_code, 'fop', 'bidask', callback_tx_bidask, state=state, webhook_manager=webhook_manager)
 quote_manager.subscribe('020039', 'stk', 'quote')
-quote_manager.add_callback('020039', 'stk', 'quote', callback_etn, state=state, webhook_manager=webhook_manager, arbitrage=arbitrage)
+quote_manager.add_callback('020039', 'stk', 'quote', callback_etn, state=state, webhook_manager=webhook_manager)
 
 loop.run_forever()
 
