0b16a1e24f3d0f8be3d2b981455172bce00a97f3
ryanovovo <ryanovovo@gmail.com>
2024-10-20 16:34:37 +0800

    Refactor callback_functions.py to use a thread-safe lock when updating state and sending embed messages

--- backend/callback_functions.py
+++ backend/callback_functions.py
@@ -1,11 +1,18 @@
+import threading
+
+state_lock = threading.RLock()
+
 
 def callback_update(data, args, kwargs):
-    try:
-        state = kwargs['state']
-    except KeyError:
-        raise ValueError("State is required")
-    state.update_frame(data)
+    updated_state = None
+    with state_lock:
+        try:
+            state = kwargs['state']
+        except KeyError:
+            raise ValueError("State is required")
+        state.update_frame(data)
+        updated_state = state
     webhook_manager = kwargs.get('webhook_manager')
     if webhook_manager is not None:
-        webhook_manager.send_embed_message(state)
+        webhook_manager.send_embed_message(updated_state)
     # pprint(dict(future_frame), sort_dicts=False)
