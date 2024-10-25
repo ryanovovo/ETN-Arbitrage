34545dc0c8a7ce54d94a9f27b29867ab57feddb2
ryanovovo <ryanovovo@gmail.com>
2024-10-25 10:56:22 +0800

    Refactor terminal.py to initialize API, set up logging, and subscribe to quote updates

---
+++ terminal.py
@@ -0,0 +1,53 @@
+# Import necessary libraries and modules
+import asyncio
+import logging
+from decimal import Decimal
+from backend.quote import QuoteManager
+from backend.utils import get_api, periodic_get_close
+from backend.frame import Frame
+from backend.state import State
+from backend.callback_functions import callback_update
+from frontend.webhook import WebhookManager
+from frontend.message import state_to_embed
+from frontend.console import ConsoleManager
+import discord
+from discord.ext import commands, tasks
+from discord.ui import Button, View
+import os
+from dotenv import load_dotenv
+import signal
+from datetime import datetime, timedelta
+import pytz
+
+# Set up logging
+logging.basicConfig(filename='./logs/shioaji.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+# Initialize API and data
+api = get_api()
+print(api.usage())
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+loop.add_signal_handler(signal.SIGINT, loop.stop)
+signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+
+quote_manager = QuoteManager(api, loop)
+console_manager = ConsoleManager()
+
+stock_code = '020039'
+future_code = 'TXFR1'
+# future_code = get_nearmonth_future_code(api, future_code)
+
+state = State(api, stock_code=stock_code, future_code=future_code)
+
+# Subscribe and set callback functions
+quote_manager.subscribe(future_code, 'fop', 'tick')
+quote_manager.subscribe(future_code, 'fop', 'bidask')
+quote_manager.subscribe(stock_code, 'stk', 'quote')
+quote_manager.add_callback(future_code, 'fop', 'tick', callback_update, state=state, console_manager=console_manager)
+quote_manager.add_callback(future_code, 'fop', 'bidask', callback_update, state=state, console_manager=console_manager)
+quote_manager.add_callback(stock_code, 'stk', 'quote', callback_update, state=state, console_manager=console_manager)
+
+
+loop.create_task(periodic_get_close(state, hours=12))
+loop.run_forever()
