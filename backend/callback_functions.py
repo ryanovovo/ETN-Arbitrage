bba3e37d932bc2e873ca106bc18fe1678579a568
ryanovovo <ryanovovo@gmail.com>
2024-10-25 10:57:49 +0800

    Refactor callback_functions.py to add callback_update_terminal for terminal updates

--- backend/callback_functions.py
+++ backend/callback_functions.py
@@ -16,3 +16,15 @@ def callback_update(data, args, kwargs):
     if webhook_manager is not None:
         webhook_manager.send_embed_message(updated_state_dict)
     # pprint(dict(state.stock_frame), sort_dicts=False)
+
+def callback_update_terminal(data, args, kwargs):
+    with state_lock:
+        try:
+            state = kwargs['state']
+        except KeyError:
+            raise ValueError("State is required")
+        state.update_frame(data)
+        updated_state_dict = dict(state)
+    console_manager = kwargs.get('console_manager')
+    if console_manager is not None:
+        console_manager.state_to_terminal(updated_state_dict)
